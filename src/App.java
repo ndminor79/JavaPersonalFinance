facba83301e2bfb9824029e897598b82e6d6f061
Nic Minor <ndminor79@gmail.com>
2025-09-06 16:25:35 -0400

    Added Initial Balance and main option screen
    -Added the ability to add an initial balance
    -Added the main menu to be placed in the main loop eventually

--- src/App.java
+++ src/App.java
@@ -1,8 +1,11 @@
 import java.util.Scanner;
 
 public class App {
+
+    public float balance = 0.0f;
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
+        float balance = 0.0f;
 
         System.out.println("Hello, Welcome to your Personal Finance App!");
         System.out.println("Please select your preffered currency: USD, EUR, GBP");
@@ -14,5 +17,22 @@ public class App {
             System.out.println("Invalid currency selection. Defaulting to USD.");
             currency = "USD";
         }
+
+        System.out.println("Please enter your initial balance in " + currency + ":");
+        balance = scanner.nextFloat();
+
+        System.out.println("Your initial balance is: " + balance + " " + currency);
+
+        System.out.println("Please Select an option:");
+        System.out.println("1. Add Income");
+        System.out.println("2. Add Expense");
+        System.out.println("3. View Balance");
+        System.out.println("4. View Transaction History");
+
+        int option = scanner.nextInt();
+
+        System.out.println("You selected option: " + option);
+
+        scanner.close();
     }
 }
